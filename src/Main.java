277dbe42f3f8cc97b05141bcc5a6dce40a1ce2a3
522730312@qq.com <522730312@qq.com>
2016-06-27 04:58:54 +0800

    TestCommit2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
+    public static void IteratorArrays(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+
+        }
+    }
     public static void main(String[] args) {
+        SortColor75 so75=new SortColor75();
+        int[] nums=new int[]{1,0,2,0,1};
+        so75.sortColors(nums);
+        IteratorArrays(nums);
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
 }
---
+++ src/SortColor75.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dikongfeixing on 16/6/27.
+ */
+public class SortColor75 {
+    public void sortColors(int[] nums) {
+
+    }
+}
